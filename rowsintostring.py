a0feabf75875e8b35e4e9c0164a20ced28125f31
njbsb <49741741+njbsb@users.noreply.github.com>
2020-04-14 15:14:22 +0800

    get first col using index instead of using name

--- rowsintostring.py
+++ rowsintostring.py
@@ -1,11 +1,17 @@
 import pandas as pd
-filepathname = 'cv.xlsx'  # replace path/name here
+# replace path/name here
+filepathname = r'D:\Documents\Python\project-2\database\input\staff_id(1).xlsx'
 file = pd.ExcelFile(filepathname)
-df = pd.read_excel(file, sheet_name='Sheet2', usecols='A')
+df = pd.read_excel(file, sheet_name='Sheet1', usecols='A')
 df = df.applymap(str)
 print(df)
-s = df['ID'].str.cat(sep=",")
-print(type(df['ID']))
-id_textfile = open("id_text.txt", "w")
+# s = df['ID'].str.cat(sep=",")
+s = df.iloc[:, 0].str.cat(sep=",")  # get the first column
+# print(s)
+id_textfile = open("staff_id_text(1).txt", "w")
 id_textfile.write(s)
 id_textfile.close()
+
+
+def IDlistToString(filepath, df):
+    pass
