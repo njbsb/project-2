c8c629f74f59bd88f00bfa536eb85413c47afb21
njbsb <49741741+njbsb@users.noreply.github.com>
2020-04-21 22:14:57 +0800

    used to download pic

--- downloadfile.py
+++ downloadfile.py
@@ -1,11 +1,12 @@
 import urllib.request
 
-id = [1054700, 1047239, 1046975, 1046260]
+id = [105363, 100296, 103390, 121862, 114164, 122791, 119607, 116893, 104567, 104827, 115405, 100886, 107732, 133834, 116090, 126205, 124622, 127157, 125131
+      ]
 
 for i in id:
-    filename = str(i) + ".pdf"
+    filename = str(i) + ".jpg"
     filelink = "https://talentengine.petronas.com/api/resources/staff/" + \
-        str(i) + "/cv"
+        str(i) + "/picture"
     urllib.request.urlretrieve(
         filelink, filename)
 
